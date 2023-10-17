a41b370f215bb66afcaaa094cd2856ca7bdde94a
Ferhat <ucarferhat3@gmail.com>
2023-10-17 15:22:09 +0200

    handle form error

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -1,8 +1,9 @@
 import { useRouter } from "next/navigation";
 import { Controller, useForm } from "react-hook-form";
 import axios from "axios";
-import { Button, TextField } from "@radix-ui/themes";
+import { Button, Callout, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
+import { useState } from "react";
 
 type IssueForm = {
   title: string;
@@ -11,27 +12,39 @@ type IssueForm = {
 const IssueForm = () => {
   const router = useRouter();
   const { register, control, handleSubmit } = useForm<IssueForm>();
+  const [error, setError] = useState("");
 
   const onSubmit = handleSubmit(async (data) => {
-    await axios.post("/api/issues", data);
-    router.push("/issues");
+    try {
+      await axios.post("/api/issues", data);
+      router.push("/issues");
+    } catch (err) {
+      setError("Error occurred");
+    }
   });
 
   return (
-    <form className="max-w-xl space-y-3" onSubmit={onSubmit}>
-      <TextField.Root>
-        <TextField.Input placeholder="title" {...register("title")} />
-      </TextField.Root>
-      <Controller
-        render={({ field }) => (
-          <SimpleMDE placeholder="Description" {...field} />
-        )}
-        control={control}
-        name="description"
-      />
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form className="max-w-xl space-y-3" onSubmit={onSubmit}>
+        <TextField.Root>
+          <TextField.Input placeholder="title" {...register("title")} />
+        </TextField.Root>
+        <Controller
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+          control={control}
+          name="description"
+        />
 
-      <Button>Submit</Button>
-    </form>
+        <Button>Submit</Button>
+      </form>
+    </div>
   );
 };
 
