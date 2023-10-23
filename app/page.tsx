21afe8752838555e753de0e7df6aaf08e91e35cb
Ferhat <ucarferhat3@gmail.com>
2023-10-23 14:23:18 +0200

    add bar chart compponent

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,35 @@
-import React from "react";
-import LatestIssues from "@/app/LatestIssues";
+import prisma from "@/prisma/client";
+import IssueSummary from "./IssueSummary";
+import LatestIssues from "./LatestIssues";
+import IssueChart from "./IssueChart";
+import { Flex, Grid } from "@radix-ui/themes";
+import { Metadata } from "next";
+import { Status } from "@prisma/client";
 
-const Home = () => {
-  return <LatestIssues />;
+const getIssueStatus = (status: Status) =>
+  prisma.issue.count({
+    where: { status },
+  });
+
+export default async function Home() {
+  const open = await getIssueStatus("OPEN");
+  const inProgress = await getIssueStatus("IN_PROGRESS");
+  const closed = await getIssueStatus("CLOSED");
+
+  return (
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Flex direction="column" gap="5">
+        <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+        <IssueChart open={open} inProgress={inProgress} closed={closed} />
+      </Flex>
+      <LatestIssues />
+    </Grid>
+  );
+}
+
+export const dynamic = "force-dynamic";
+
+export const metadata: Metadata = {
+  title: "Issue Tracker - Dashboard",
+  description: "View a summary of project issues",
 };
-
-export default Home;
