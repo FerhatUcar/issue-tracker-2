35b2226cf30b6d9bf5c3ebc730cf9f3d244cf4c6
Ferhat <ucarferhat3@gmail.com>
2023-11-03 15:54:49 +0100

    update export client var

--- prisma/client.ts
+++ prisma/client.ts
@@ -10,8 +10,8 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClientSingleton | undefined;
 };
 
-const client = globalForPrisma.prisma ?? prismaClientSingleton();
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 
-export default client;
+export default prisma;
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = client;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
