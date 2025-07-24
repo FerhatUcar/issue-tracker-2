edea12b500140e952dcd822493153df259af2617
Ferhat Ucar <ucarferhat3@gmail.com>
2025-07-24 21:55:59 +0200

    Disable server-side rendering for IssueForm component

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,6 +2,7 @@ import dynamic from "next/dynamic";
 import IssueFormSkeleton from "@/app/issues/_components/IssueFormSkeleton";
 
 const IssueForm = dynamic(() => import("@/app/issues/_components/IssueForm"), {
+  ssr: false,
   loading: () => <IssueFormSkeleton />,
 });
 
