778238835c835958c71fdba606d0bcefd5c0ccf2
ferhatucar <ucarferhat3@gmail.com>
2025-08-20 23:45:38 +0200

    Update spinner

--- app/components/Spinner/Spinner.tsx
+++ app/components/Spinner/Spinner.tsx
@@ -1,13 +1,11 @@
-import React from "react";
 import { Box } from "@radix-ui/themes";
+import { MdOutlineRocketLaunch } from "react-icons/md";
 
 export const Spinner = () => (
   <Box className="fixed inset-0 flex items-center justify-center z-50 bg-transparent">
-    <output
-      className="h-12 w-12 animate-spin rounded-full border-8 border-solid border-current border-r-transparent"
-      role="status"
-    >
-      <span className="sr-only">Loading...</span>
-    </output>
+    <MdOutlineRocketLaunch
+      className="h-12 w-12 text-sky-500 animate-rocket"
+      aria-label="Loading..."
+    />
   </Box>
 );
