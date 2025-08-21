043d3fba84b7558310fac76f8c5f94f0ce73bd17
ferhatucar <ucarferhat3@gmail.com>
2025-08-21 14:43:17 +0200

    fix issue route

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -36,7 +36,7 @@ export async function POST(request: NextRequest) {
       assignedToUser: assignedToUserId
         ? { connect: { id: assignedToUserId } }
         : undefined,
-      Workspace: {
+      workspace: {
         connect: { id: workspaceId ?? "" },
       },
     },
